cfc36cf9f2b146abbddba8b0c515f8a06f1eeb26
EGURGINE <sdh210202@sdh.hs.kr>
2022-01-20 16:05:50 +0900

    hp

---
+++ Assets/Junho/Script/PlayerControl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Jump");
+
+        transform.Translate(new Vector2(x, y) * Time.deltaTime * speed);
+    }
+}
