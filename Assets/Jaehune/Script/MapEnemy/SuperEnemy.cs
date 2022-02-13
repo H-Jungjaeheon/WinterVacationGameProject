d8faba523f2aee941e48db7f0faa81124e92b4b7
H-Jungjaeheon <jjhking222@naver.com>
2022-02-13 19:24:14 +0900

    BasicEnemyAllMake
    BasicEnemyAllMake1

--- Assets/Jaehune/Script/MapEnemy/SuperEnemy.cs
+++ Assets/Jaehune/Script/MapEnemy/SuperEnemy.cs
@@ -4,19 +4,79 @@ using UnityEngine;
 
 public class SuperEnemy : BasicEnemyScript
 {
-    // Start is called before the first frame update
+    [SerializeField] float SkillCount;
+    [SerializeField] GameObject WarningCircleObj, SkillCircleObj;
+    [SerializeField] bool Skillng, Movings;
+
     public override void Start()
     {
         base.Start();
     }
-    // Update is called once per frame
     public override void Update()
     {
-        base.Update();
+        RayCasting();
+        Moving();
+        if (IsMove == false)
+        {
+            MoveCount = 0;
+        }
+        if (IsFind == true)
+        {
+            ManaSkill();
+            FindPlayer();
+            CrossroadPlus();
+            CrossRoadObj.SetActive(false);
+        }
+        else
+        {
+            IsMove = true;
+            Skillng = false;
+            SkillCount = 0;
+            SkillCircleObj.SetActive(false);
+            CrossRoadObj.SetActive(true);
+            IsPlus = 1;
+        }
+        if (SkillCount >= 5 && Skillng == false)
+        {
+            WarningCircleObj.SetActive(true);
+            Invoke("UseSkill", 2.5f);
+            IsMove = false;
+            Movings = false;
+        }
+        else
+        {
+            Movings = true;
+        }
+    }
+    void ManaSkill()
+    {
+        SkillCount += Time.deltaTime;
+    }
+    void UseSkill()
+    {
+        if (Player != null && GameManager.Instance.IsBattleStart == false && GameManager.Instance.isEunsin == false)
+        {
+            Skillng = true;
+            IsMove = true;
+            Movings = true;
+            WarningCircleObj.SetActive(false);
+            SkillCircleObj.SetActive(true);
+        }
     }
     public override void Moving()
     {
-        base.Moving();
+        if (IsMove == true && Movings == true)
+        {
+            MoveCount += Time.deltaTime;
+            transform.position += new Vector3(Speed * Time.deltaTime, 0, 0);
+            if (MoveCount >= MaxMoveCount)
+            {
+                animator.SetBool("IsIdle", true);
+                MoveCount = 0;
+                IsMove = false;
+                Invoke("Trun", 4f);
+            }
+        }
     }
     public override void Trun()
     {
@@ -28,7 +88,15 @@ public class SuperEnemy : BasicEnemyScript
     }
     public override void FindPlayer()
     {
-        base.FindPlayer();
+        MoveCount = 0;
+        if (Speed > 0 && IsMove == true && GameManager.Instance.isEunsin == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position + new Vector3(0, 0.15f, 0), Speed * 3f * Time.deltaTime);
+        }
+        else if (Speed < 0 && IsMove == true && GameManager.Instance.isEunsin == false)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position + new Vector3(0, 0.15f, 0), Speed * -3f * Time.deltaTime);
+        }
     }
     public override void Delete()
     {
@@ -40,6 +108,6 @@ public class SuperEnemy : BasicEnemyScript
     }
     public override void CrossroadPlus()
     {
-        base.CrossroadPlus();
+        IsPlus = 6;
     }
 }
