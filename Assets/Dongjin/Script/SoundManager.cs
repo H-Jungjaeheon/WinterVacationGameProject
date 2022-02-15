a3012684d9efed975bcbd828cb87771c1d11c966
kgoon <sdh210203@sdh.hs.kr>
2022-02-15 23:09:56 +0900

    사운드 매니저

--- Assets/Dongjin/Script/SoundManager.cs
+++ Assets/Dongjin/Script/SoundManager.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource[] musicSource;
+    public AudioSource[] SoundSource;
+    void Awake()
     {
-        
+        for (int i = 0; i < musicSource.Length; i++)
+            musicSource[i].volume = GameObject.Find("BGMusic").GetComponent<Slider>().value;
+        for (int i = 0; i < SoundSource.Length; i++)
+            SoundSource[i].volume = GameObject.Find("SEMusic").GetComponent<Slider>().value;
     }
-
-    // Update is called once per frame
-    void Update()
+    public void SetMusicVolume(float volume)
     {
-        
+        for(int i = 0; i<musicSource.Length;i++)
+        {
+           musicSource[i].volume = volume;  
+        }
+    }
+    public void SetSoundEffect(float volume)
+    {
+        for (int i = 0; i < SoundSource.Length; i++)
+        {
+            SoundSource[i].volume = volume;
+        }
     }
 }
