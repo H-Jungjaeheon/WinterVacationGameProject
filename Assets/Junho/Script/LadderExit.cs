bced41d527017ecc1d38fc5cb2965cff80012710
EGURGINE <sdh210202@sdh.hs.kr>
2022-02-17 00:03:59 +0900

    사다리

--- Assets/Junho/Script/LadderExit.cs
+++ Assets/Junho/Script/LadderExit.cs
@@ -6,48 +6,57 @@ public class LadderExit : MonoBehaviour
 {
     public Collider2D col;
     public GameObject Interaction;
-
+    public Sprite up;
+    public bool isPlayer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        isPlayer = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isPlayer&&Input.GetKey(KeyCode.F))
+        {
+            GameManager.Instance.is2F = true;
+            Physics2D.IgnoreCollision(GameObject.Find("Player").GetComponent<Collider2D>(), col, true);
+        }
     }
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
+            isPlayer=true;
             Interaction.SetActive(true);
 
-
         }
     }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            if (Input.GetKey(KeyCode.F))
-            {
 
-                Physics2D.IgnoreCollision(collision.GetComponent<Collider2D>(), col, true);
-            }
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+    //    if (collision.CompareTag("Player"))
+    //    {
+    //        if (Input.GetKey(KeyCode.F))
+    //        {
+    //            GameManager.Instance.is2F = true;
+    //            Physics2D.IgnoreCollision(collision.GetComponent<Collider2D>(), col, true);
+    //        }
 
-        }
-    }
+    //    }
+    //}
     private void OnTriggerExit2D(Collider2D collision)
     {
-
-
+        Debug.Log("d");
         Physics2D.IgnoreCollision(collision.GetComponent<Collider2D>(), col, false);
+        
         if (collision.CompareTag("Player"))
         {
             Interaction.SetActive(false);
 
-
+            isPlayer = false;
+            GameManager.Instance.is2F = false;
         }
 
 
