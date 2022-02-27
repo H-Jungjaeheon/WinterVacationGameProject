6880c1a04ac778ac9aab8a4108400f5c5ae69255
H-Jungjaeheon <jjhking222@naver.com>
2022-02-27 15:50:14 +0900

    BattleBugFixed
    BattleBugFixed1

--- Assets/Jaehune/Script/BattleEnemy/BattleSuperEnemy.cs
+++ Assets/Jaehune/Script/BattleEnemy/BattleSuperEnemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BattleSuperEnemy : BattleBasicEnemy
 {
@@ -20,6 +21,7 @@ public class BattleSuperEnemy : BattleBasicEnemy
         MaxHp *= GameManager.Instance.Stage;
         Hp *= GameManager.Instance.Stage;
         SRS = this.GetComponent<SpriteRenderer>();
+        SR = this.GetComponent<SpriteRenderer>();
         this.transform.position = EnemySpawner.transform.position + new Vector3(0f, 2.1f, 0);
         SkillAttackRand = Random.Range(1, 4);
     }
